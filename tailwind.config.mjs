71bbf79c8f6e63a0b63ad03d0834e3731db3e761
maopadev <dev.maopa@gmail.com>
2023-12-21 17:22:57 +0100

    Fix - Update the data to adjust with my profile

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,md,mdx,ts,tsx}'],
+	enabled: process.env.NODE_ENV === 'production',
+	safeList: [],
+	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
