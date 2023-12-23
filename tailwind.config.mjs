7cce4809bfce08a1244d288078cc8717b0b03f82
maopadev <dev.maopa@gmail.com>
2023-12-23 19:38:20 +0100

    Fix- Se posiciona correctamente algunos archivos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,md,mdx,ts,tsx}'],
-	enabled: process.env.NODE_ENV === 'production',
+	content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
+	enabled: process.env.NODE_ENV === "production",
 	safeList: [],
-	darkMode: 'class',
+	darkMode: "class",
 	theme: {
-		extend: {},
+		fontFamily: {
+			"termina-heavy": ["Termina Heavy", "sans-serif"],
+			"termina-bold": ["Termina Bold", "sans-serif"],
+			"bricolage": ["Bricolage Grotesque", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				primary: "#0d0d0d",
+				secondary: "#f2f2f2",
+				"accent-one": "#A5FFC9",
+				"accent-two": "#FCFABC",
+			},
+		},
 	},
 	plugins: [],
-}
+};
