9111af480f9cc1a548acef003c6385d5e91a7d5e
maopadev <dev.maopa@gmail.com>
2023-12-23 19:39:40 +0100

    Add - Se añaden nuevas constantes, luego iran en el db

---
+++ src/hooks/consts.ts
@@ -0,0 +1,83 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "maopadev";
+export const SITE_DESCRIPTION = "Welcome to my website!";
+
+export const LIST_TAGS = [
+  { id: 1, title: "Java", color: "bg-[#eeb530]" },
+  { id: 2, title: "Spring Boot", color: "bg-[#]" },
+  { id: 3, title: "Angular", color: "bg-[#]" },
+  { id: 4, title: "React", color: "bg-[#]" },
+  { id: 6, title: "Javascript", color: "bg-[#]" },
+  { id: 7, title: "Typescript", color: "bg-[#]" },
+  { id: 11, title: "Bootstrap", color: "bg-[#]" },
+  { id: 15, title: "PL/SQL", color: "bg-[#]" },
+  { id: 16, title: "MySQL", color: "bg-[#]" },
+  { id: 18, title: "Git", color: "bg-[#]" },
+  { id: 19, title: "GitHub", color: "bg-[#]" },
+  { id: 20, title: "GitLab", color: "bg-[#]" },
+  { id: 22, title: "Jira", color: "bg-[#]" },
+  { id: 24, title: "Docker", color: "bg-[#]" },
+  { id: 25, title: "Kubernetes", color: "bg-[#]" },
+  { id: 26, title: "AWS", color: "bg-[#]" },
+  { id: 27, title: "Azure", color: "bg-[#]" },
+  { id: 28, title: "JPA", color: "bg-[#]" },
+  { id: 29, title: "Spring MVC", color: "bg-[#]" },
+  { id: 30, title: "Hibernate", color: "bg-[#]" },
+  { id: 31, title: "Maven", color: "bg-[#]" },
+  { id: 32, title: "JQuery", color: "bg-[#]" },
+  { id: 33, title: "Pruebas unitarias", color: "bg-[#]" },
+  { id: 34, title: "IntelliJ Idea", color: "bg-[#]" },
+  { id: 35, title: "Tomcat", color: "bg-[#]" },
+  { id: 36, title: "Postman", color: "bg-[#]" },
+  { id: 37, title: "Jenkins", color: "bg-[#]" },
+  { id: 38, title: "Sonarlint", color: "bg-[#]" },
+  { id: 39, title: "Junit", color: "bg-[#]" },
+  { id: 40, title: "Mockito", color: "bg-[#]" },
+  { id: 47, title: "Eclipse", color: "bg-[#]" },
+  { id: 48, title: "Mantenimiento de Base de Datos", color: "bg-[#]" },
+  { id: 49, title: "REST", color: "bg-[#]" },
+  { id: 50, title: "SOAP", color: "bg-[#]" },
+  { id: 51, title: "Subversion", color: "bg-[#]" },
+];
+
+export const LIST_EXPERENCIES = [
+  {
+    id: 2,
+    job: "Full Stack Developer",
+    description: "",
+    company: ["SearchIT", ``],
+    period: [202112, "Present"],
+    tags: [1, 28, 39, 2, 29, 30, 31, 16, 3, 4, 37, 38, 35, 34, 50, 49, 51, 20],
+  },
+  {
+    id: 1,
+    job: "Junior Java Developer",
+    description: "",
+    company: ["Minsait", ``],
+    period: [201910, 202111],
+    tags: [1, 28, 2, 29, 30, 31, 32, 33],
+  },
+];
+
+export const JSON_LIST_BUTTONS_SALUTE = [
+  {
+    id: 1,
+    text: "Descargar CV",
+    href: ``,
+    color: "bg-accent-one",
+  },
+  {
+    id: 2,
+    text: "AA",
+    href: ``,
+    color: "bg-accent-two",
+  },
+];
+
+export const JSON_ABOUT_TEXT_SALUTE = [
+  "Actualmente estoy en SearchIT creando y realizando el mantenimiento de aplicaciones web para diferentes clientes en colaboración con esta empresa.",
+  "Me gusta crear aplicaciones cautivadoras y visualmente impactantes usando las herramientas de desarrollo como React, Astro, Java, JSP, Figma, ... logrando aplicaciones atractivas y funcionales.",
+  `“Me considero una alguien que consigue lo que se propone, ya que creo que los logros son los resultados de las decisiones que se toman “`,
+];
