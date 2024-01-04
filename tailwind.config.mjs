b7dea8351e7a16abc9319ef9892fe974abac23fc
maopadev <dev.maopa@gmail.com>
2024-01-04 10:38:26 +0100

    Add - Se agrega la marquesina de proyectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,20 @@ export default {
         "accent-one": "#A5FFC9",
         "accent-two": "#FCFABC",
       },
+      animation: {
+        marquee: 'marquee 30s linear infinite',
+        marquee2: 'marquee2 30s linear infinite'
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' }
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' }
+        }
+      }
     },
   },
   plugins: [],
