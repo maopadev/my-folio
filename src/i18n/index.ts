35ecfa7210e2251b316d06107071be234e80dddc
maopadev <dev.maopa@gmail.com>
2024-01-10 23:33:39 +0100

    Fix - Remove unused i18n files and update component styles

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,16 +1,25 @@
-import HomeSpanish from "@i18n/home/es.json";
-import HomeEnglish from "@i18n/home/en.json";
-import HomePortuguese from "@i18n/home/pt.json";
 
+/// Languages Actives
 const LANGUAGES_ACTIVES = {
   SPANISH: "es",
   ENGLISH: "en",
   PORTUGUESE: "pt",
 };
 
+// Home Page Internationalization
+import HomeSpanish from "@i18n/home/es.json";
+import HomeEnglish from "@i18n/home/en.json";
+import HomePortuguese from "@i18n/home/pt.json";
+
+const { SPANISH, ENGLISH, PORTUGUESE } = LANGUAGES_ACTIVES;
+
+/**
+ * Get the home data according to the language
+ * @param currentLocale
+ */
 export const getHomeDataInternational = ({ currentLocale }: { currentLocale: string | undefined }) => {
-  if (currentLocale === LANGUAGES_ACTIVES.SPANISH) return HomeSpanish;
-  if (currentLocale === LANGUAGES_ACTIVES.ENGLISH) return HomeEnglish;
-  if (currentLocale === LANGUAGES_ACTIVES.PORTUGUESE) return HomePortuguese;
+  if (currentLocale === SPANISH) return HomeSpanish;
+  if (currentLocale === ENGLISH) return HomeEnglish;
+  if (currentLocale === PORTUGUESE) return HomePortuguese;
   return HomeSpanish;
 };
